83eab4a9e9a20003e9eaab79112b188eb72c415b
ShishirRijal <ccrrizal0892@gmail.com>
2022-02-05 11:55:57 +0545

    ui completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/constant.dart';
+// import 'package:expense_tracker/screens/homepage.dart';
 import 'package:expense_tracker/screens/singup_screen.dart';
 import 'package:expense_tracker/widgets/bottom_text.dart';
 import 'package:expense_tracker/widgets/flat_text_button.dart';
@@ -12,81 +13,82 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(
-              left: 20.0,
-              right: 20.0,
-              bottom: MediaQuery.of(context).viewPadding.bottom + 10),
-          alignment: Alignment.center,
-          height: double.infinity,
-          width: double.infinity,
+      body: Center(
+        child: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Welcome back!",
-                  style: kColouredHeadline,
-                ),
-                const SizedBox(height: 20.0),
-                const Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    "Login",
-                    style: kHeadline,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 20.0,
+                  right: 20.0,
+                  bottom: MediaQuery.of(context).viewPadding.bottom + 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Welcome back!",
+                    style: kColouredHeadline,
                   ),
-                ),
-                const SizedBox(height: 30.0),
-                const Text(
-                  "E-mail",
-                  style: kTitleTextStyle,
-                ),
-                const SizedBox(height: 10.0),
-                UnderLinedInputField(
-                  controller: usernameController,
-                  hintText: 'Your e-mail here',
-                ),
-                const SizedBox(height: 20.0),
-                const Text(
-                  "Password",
-                  style: kTitleTextStyle,
-                ),
-                const SizedBox(height: 10.0),
-                UnderLinedInputField(
-                  controller: passwordController,
-                  hintText: 'Your password here',
-                  hideText: true,
-                ),
-                const SizedBox(height: 50.0),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: FlatTextButton(
-                      title: "Login",
-                      onClick: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return SignupScreen();
-                        }));
-                      }),
-                ),
-                const SizedBox(height: 20.0),
-                BottomText(
-                  title: "Don't have an account yet?",
-                  text: "Sign Up",
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return SignupScreen();
-                        },
-                      ),
-                    );
-                  },
-                )
-              ],
+                  const SizedBox(height: 20.0),
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "Login",
+                      style: kHeadline,
+                    ),
+                  ),
+                  const SizedBox(height: 30.0),
+                  const Text(
+                    "E-mail",
+                    style: kTitleTextStyle,
+                  ),
+                  const SizedBox(height: 10.0),
+                  UnderLinedInputField(
+                    controller: usernameController,
+                    hintText: 'Your e-mail here',
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    "Password",
+                    style: kTitleTextStyle,
+                  ),
+                  const SizedBox(height: 10.0),
+                  UnderLinedInputField(
+                    controller: passwordController,
+                    hintText: 'Your password here',
+                    hideText: true,
+                  ),
+                  const SizedBox(height: 50.0),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: FlatTextButton(
+                        title: "Login",
+                        onClick: () {
+                          // Navigator.push(context,
+                          //     MaterialPageRoute(builder: (context) {
+                          //   return HomePage(
+                          //     username: usernameController.text.trim(),
+                          //   );
+                          // }));
+                        }),
+                  ),
+                  const SizedBox(height: 20.0),
+                  BottomText(
+                    title: "Don't have an account yet?",
+                    text: "Sign Up",
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return SignupScreen();
+                          },
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
